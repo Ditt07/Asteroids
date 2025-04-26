8ef22baaf07cf9b59140f05ee97485efb67b5a31
Ditt07 <stevsr0705@gmail.com>
2025-04-26 16:16:16 +0200

    Player update

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame
 from constants import *
-
-Time = pygame.time.Clock()
-dt = 0
+import player
 
 def main ():
     pygame.init()
+    Time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player = player.Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        Player.draw(screen)
         pygame.display.flip()
+        
         dt = 1000/Time.tick(60)
 
 if __name__=="__main__":
